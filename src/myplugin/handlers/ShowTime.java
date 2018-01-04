d68ac241b249a0885ab6724debdf83d6ad2acdaf
zhangboad <zhangboad@enn.cn>
2018-01-04 10:12:24 +0800

    init

---
+++ src/myplugin/handlers/ShowTime.java
@@ -0,0 +1,28 @@
+package myplugin.handlers;
+
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IStartup;
+
+/**
+ * @author zhangbo
+ *
+ */
+public class ShowTime implements IStartup {
+
+  @Override
+  public void earlyStartup() {
+    Display.getDefault().syncExec(new Runnable() {
+      public void run() {
+        long eclipseStartTime = Long.parseLong(System.getProperty("eclipse.startTime"));
+        long costTime = System.currentTimeMillis() - eclipseStartTime;
+        Shell shell = Display.getDefault().getActiveShell();
+        String message = "Eclipse start in " + costTime + "ms";
+        MessageDialog.openInformation(shell, "Information", message);
+      }
+    });
+
+  }
+
+}
